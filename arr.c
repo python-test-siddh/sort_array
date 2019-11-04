4034d1eccb6d39fae367ed82e79a397907a578ac
siddht4 <dhar.75.ap@gmail.com>
2019-11-04 16:49:13 +0530

    Update arr.c

--- arr.c
+++ arr.c
@@ -2,29 +2,11 @@
 #include <Python.h>
 
 // wrapper function for moku
-static PyObject *Moku(PyObject *self, PyObject *args) {
+static PyObject *Moku(PyObject *self, PyObject *args) 
+{
     PyObject *lst;
-    if (!PyArg_ParseTuple(args, "O", &lst)) {
-        return NULL;
-    }
-
-    int n = PyObject_Length(lst);
-    if (n < 0) 
-    {
-        return NULL;
-    }
-
-    long nums[n];
-    for (int i = 0; i < n; i++) {
-        PyLongObject *item = PyList_GetItem(lst, i);
-        long num = PyLong_AsLong(item);
-        nums[i] = num;
-    }
-
-    unsigned long fact_sum;
-    fact_sum = ifactorial_sum(nums, n);
-
-    return Py_BuildValue("i", fact_sum);
+    
+    return lst;
 }
 
 // module's function table
